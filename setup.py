aff14be2f2786ef068c1b19bb2f44b8a18e09bb9
cylos9 <bobdbl99@gmail.com>
2023-02-03 23:37:48 +0900

    update python module's name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['my_turtlebot'],
+    packages=['alexa_ros_interface'],
     package_dir={'': 'src'},
 )
 
